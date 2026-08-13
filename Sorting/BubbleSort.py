527eb0d1d20f82f8e940395d0baa8a036fc970e3
Nguyen Hien Long <ngnhnlng@gmail.com>
2026-08-13 20:39:14 +0700

    Implement bubble sort

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,1 +1,11 @@
+def BubbleSort(Ds):
+    N = len(Ds)
+    for i in range(N):
+        for j in range(0, N - i -1):
+            if Ds[j] > Ds[j+1]:
+                Ds[j], Ds[j+1] = Ds[j+1], Ds[j]
+    return Ds
 
+Ds = [5, 3, 8, 4, 2]
+
+print(BubbleSort(Ds))
